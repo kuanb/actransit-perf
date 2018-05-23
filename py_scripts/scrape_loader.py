eb7d0473200669dd9bee3d72685b088c485f937c
Kuan Butts <kuanbutts@gmail.com>
2018-05-22 22:45:47 -0700

    wip

---
+++ py_scripts/scrape_loader.py
@@ -0,0 +1,13 @@
+import subprocess
+import os
+
+# First, get all directories in busdata
+main_dir = 'busdata'
+all_day_directories = [g for g in os.walk(main_dir)][0][1]
+
+for day_dir in all_day_directories:
+    full_day_dir_path = os.path.join([main_dir, day_dir])
+    print('full_day_dir_path', full_day_dir_path)
+
+# args = ['echo', 'Hello!']
+# subprocess.Popen(args)
