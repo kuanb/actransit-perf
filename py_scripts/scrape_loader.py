f03c58d664b96c34af0e27384625ae4e67c5bf7e
Kuan Butts <kuanbutts@gmail.com>
2018-05-22 23:07:29 -0700

    wip

--- py_scripts/scrape_loader.py
+++ py_scripts/scrape_loader.py
@@ -5,9 +5,15 @@ import os
 main_dir = 'busdata'
 all_day_directories = [g for g in os.walk(main_dir)][0][1]
 
+# Craft a full list of paths to upload
+to_upload = []
 for day_dir in all_day_directories:
     full_day_dir_path = os.path.join(main_dir, day_dir)
-    print('full_day_dir_path: ', full_day_dir_path)
+
+    for filename in os.listdir(full_day_dir_path):
+        if filename.endswith('.json'):
+            print('filename', filename)
+            # to_upload.append(to_upload)
 
 # args = ['echo', 'Hello!']
 # subprocess.Popen(args)
