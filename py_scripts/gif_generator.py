76ccaf14a68723857ecce5b01ab534ad40d3c761
Kuan Butts <kuanbutts@gmail.com>
2018-05-29 16:53:27 -0700

    wip

--- py_scripts/gif_generator.py
+++ py_scripts/gif_generator.py
@@ -340,7 +340,7 @@ def tweet(gif_loc):
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
     api = tweepy.API(auth)
-    api.update_with_media(gif_loc, status='')
+    api.update_with_media(gif_loc)
 
 
 # Run when this script is invoked
